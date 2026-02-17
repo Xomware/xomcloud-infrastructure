78af36b44602eaa17e147dae9df28cbfbce71848
Dominick Giordano <dominickgiordano@Dominicks-MacBook-Pro.local>
2026-02-17 13:54:17 -0500

    Refactor

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,21 +1,21 @@
 resource "aws_lambda_function" "download_tracks" {
-  function_name     = "${var.app_name}-download-tracks"
-  description       = "Lambda Function for downloading soundcloud tracks, zipping, uploading to s3 and returning a presigned url."
-  package_type      = "Image"
-  architectures     = ["x86_64"]
-  image_uri         = "${aws_ecr_repository.download_tracks.repository_url}:latest"
-  memory_size       = 1024
-  timeout           = 300
-  role              = aws_iam_role.lambda_role.arn
+  function_name = "${var.app_name}-download-tracks"
+  description   = "Lambda Function for downloading soundcloud tracks, zipping, uploading to s3 and returning a presigned url."
+  package_type  = "Image"
+  architectures = ["x86_64"]
+  image_uri     = "${aws_ecr_repository.download_tracks.repository_url}:latest"
+  memory_size   = 1024
+  timeout       = 300
+  role          = aws_iam_role.lambda_role.arn
   environment {
     variables = merge(local.lambda_variables, { S3_DOWNLOAD_BUCKET_NAME = aws_s3_bucket.downloads.id })
   }
 
   image_config {
-    command = ["lambdas.download_tracks.handler.handler"] 
+    command = ["lambdas.download_tracks.handler.handler"]
   }
 
-  tags = merge(local.standard_tags, tomap({"name" = "${var.app_name}-download-tracks"}))
+  tags = merge(local.standard_tags, tomap({ "name" = "${var.app_name}-download-tracks" }))
 
   tracing_config {
     mode = var.lambda_trace_mode
--- terraform/lambda_authorizer.tf
+++ terraform/lambda_authorizer.tf
@@ -1,23 +1,23 @@
 ## Resources for API Gateway Lambda Authorization
 resource "aws_lambda_function" "authorizer" {
-  function_name     = "${var.app_name}-authorizer"
-  description       = "Lambda Authorizer for ${var.app_name}"
-  package_type      = "Image"
-  architectures     = ["x86_64"]
-  image_uri         = "${aws_ecr_repository.authorizer.repository_url}:latest"
-  memory_size       = 256
-  timeout           = 30
-  role              = aws_iam_role.lambda_role.arn
+  function_name = "${var.app_name}-authorizer"
+  description   = "Lambda Authorizer for ${var.app_name}"
+  package_type  = "Image"
+  architectures = ["x86_64"]
+  image_uri     = "${aws_ecr_repository.authorizer.repository_url}:latest"
+  memory_size   = 256
+  timeout       = 30
+  role          = aws_iam_role.lambda_role.arn
 
   environment {
     variables = local.lambda_variables
   }
 
   image_config {
-    command = ["lambdas.authorizer.handler.handler"] 
+    command = ["lambdas.authorizer.handler.handler"]
   }
 
-  tags = merge(local.standard_tags, tomap({"name" = "${var.app_name}-authorizer"}))
+  tags = merge(local.standard_tags, tomap({ "name" = "${var.app_name}-authorizer" }))
 
   tracing_config {
     mode = var.lambda_trace_mode
@@ -36,21 +36,4 @@ resource "aws_lambda_function" "authorizer" {
     aws_iam_role.lambda_role
   ]
 
-}
-resource "aws_api_gateway_authorizer" "lambda_authorizer" {
-  name                   = "${var.app_name}-authorizer"
-  rest_api_id            = aws_api_gateway_rest_api.api_gateway.id
-  authorizer_uri         = aws_lambda_function.authorizer.invoke_arn
-  authorizer_credentials = aws_iam_role.lambda_role.arn
-  type                   = "TOKEN"
-  identity_source        = "method.request.header.Authorization"
-  authorizer_result_ttl_in_seconds = 300
-}
-
-#Give api gateway permission to invoke lambda authorizer
-resource "aws_lambda_permission" "lambda_authorizer_handler_permission" {
-  statement_id  = "AllowExecFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.authorizer.function_name
-  principal     = "apigateway.amazonaws.com"
 }
